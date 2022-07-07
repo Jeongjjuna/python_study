63d466686a0d731e8f9b5f0117875665f1dd85bc
Jeongjihun <wlgns2234@naver.com>
2022-07-07 13:21:12 +0900

    제어플로와코드구조~ing

--- 제어플로와코드구조.py
+++ 제어플로와코드구조.py
@@ -174,3 +174,27 @@ def outer(a, b):
 
 outer(1, 2)
 
+
+#26. 클로저(고도의 파이썬 기술! 좀 어려움)
+def outer(a, b):
+    def inner():
+        return a + b
+    return inner
+
+f = outer(1, 2)
+r = f()
+print(r)
+
+'''사용 예시(용도에 따라 나눠서 사용가능)'''
+def circle_area_func(pi):
+    def circle_area(radius):
+        return pi * radius * radius
+    
+    return circle_area
+
+cal1 = circle_area_func(3.14)
+cal2 = circle_area_func(3.141592)
+
+print(cal1(10))
+print(cal2(10))
+
