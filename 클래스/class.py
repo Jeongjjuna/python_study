a9fae001cf945be206496f2b4f43dd4cd9d27e7c
Jeongjihun <wlgns2234@naver.com>
2022-07-10 12:10:14 +0900

    객체와클래스~ing

--- 클래스/class.py
+++ 클래스/class.py
@@ -52,3 +52,31 @@ print("##########")
 tesla_car = TeslaCar()
 tesla_car.run()
 tesla_car.auto_run()
+
+
+#4. 메소드 오버라이드(재정의 덮어쓰기) 및 super().__init__
+class Car:
+    def __init__(self, model, color):
+        self.model = model
+        self.color = color
+
+    def run(self):
+        print('run')
+
+
+class ToyotaCar(Car):
+    def __init__(self, model, color, year):
+        super().__init__(model, color) #두개는 이전 것을 사용하겠다
+        self.year = year #ToyotaCar에서만 사용하는 생성자 파라미터
+    
+    def run(self): #오버라이딩
+        super().run() #부모 메소드 실행가능
+        print('fast')
+
+
+class TeslaCar(Car):
+    def run(self): #오버라이딩
+        print('super fast')
+
+a = ToyotaCar(1,2,3)
+a.run()
