e0b364069bebce882c3a8541c16058a0152eaf1a
Jeongjihun <wlgns2234@naver.com>
2022-07-12 23:55:24 +0900

    객체와클래스~ing

--- 클래스/class.py
+++ 클래스/class.py
@@ -80,3 +80,66 @@ class TeslaCar(Car):
 
 a = ToyotaCar(1,2,3)
 a.run()
+
+
+#5. Property를 사용한 속성의 설정(속성을 마음대로 바꾸기 싫을 때)
+#-------------------------------밖에서 속성을 쉽게 바꿀 수 있음---------------------
+class Car:
+    def __init__(self, model=None):
+        self.model = model
+
+    def run(self):
+        print('run')
+
+class TeslaCar(Car):
+    def __init__(self, model='Model S', enable_auto_run=False):
+        super().__init__(model)
+        self.enable_auto_run = enable_auto_run
+
+    def run(self):
+        print('super fast')
+
+
+TeslaCar = TeslaCar('Model S')
+TeslaCar.enable_auto_run = True
+print(TeslaCar.enable_auto_run)
+
+#-------------------------------밖에서 속성값을 쉽게 바꿀 수 없도록 하고 싶을 때------
+
+class Car:
+    def __init__(self, model=None):
+        self.model = model
+
+    def run(self):
+        print('run')
+
+class TeslaCar(Car):
+    def __init__(self, model='Model S',
+                        enable_auto_run=False,
+                        passwd='123'):
+        super().__init__(model)
+        #언더스코어(_)1개는 명시적으로 외부에서 속성값을 바꾸지 않았으면 좋겠다 라는 뜻이다
+        self._enable_auto_run = enable_auto_run
+        self.passwd = passwd
+
+    @property #기본적으로 못바꾸게 한다
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter #passwd값을 잘못읽으면 에러발생시키고 잘 입력시키면 True로 바꿔준다
+    def enable_auto_run(self, is_enable):
+        if self.passwd == '456':
+            self._enable_auto_run = is_enable
+        else:
+            raise ValueError
+
+    def run(self):
+        print('super fast')
+
+
+TeslaCar = TeslaCar('Model S', passwd='123')
+#TeslaCar.enable_auto_run = True #수정할 수 없다는 에러(ok)
+TeslaCar.enable_auto_run = True
+print(TeslaCar.enable_auto_run)
+
+#------------------ 언더스코어2개(__)에 관한 것은 다음에 공부---------------------
